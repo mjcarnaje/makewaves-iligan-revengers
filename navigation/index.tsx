415e174ed9b725ab1c8cc8e4f327dd47c3f6cad7
mjcarnaje <michaeljamescarnaje1@gmail.com>
2021-08-05 20:55:50 +0800

    remove not found screen

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -3,15 +3,13 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
-import NotFoundScreen from "../screens/NotFoundScreen";
 import BottomTabNavigator from "./BottomTabNavigator";
-import LinkingConfiguration from "./LinkingConfiguration";
 import { RootStackParamList } from "../types";
 import Drug from "../screens/Drug";
 
 export default function Navigation() {
   return (
-    <NavigationContainer linking={LinkingConfiguration}>
+    <NavigationContainer>
       <RootNavigator />
     </NavigationContainer>
   );
@@ -24,11 +22,6 @@ function RootNavigator() {
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Root" component={BottomTabNavigator} />
       <Stack.Screen name="Drug" component={Drug} />
-      <Stack.Screen
-        name="NotFound"
-        component={NotFoundScreen}
-        options={{ title: "Oops!" }}
-      />
     </Stack.Navigator>
   );
 }
